acb0ba275b7e533404f9b857907f75d6f4570cc6
Alex Cahn <alexcahn@gmail.com>
2017-02-09 23:45:27 -0800

    got a template with 2 articles

---
+++ JS/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var articles = [];
+
+function Article(opts) {
+  // TODO: Use the JS object passed in to complete this constructor function:
+  // Save ALL the properties of `opts` into `this`
+
+  this.title = opts.title;
+  this.category = opts.category;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function () {
+  var $newArticle = $('article.template').clone();
+  /* TODO: This cloned article is no longer a template,
+  as it now has real data attached to it! We need to account
+  for that before this current article gets rendered to our
+  DOM. */
+  $newArticle.removeClass('template');
+
+  if (!this.publishedOn) $newArticle.addClass('draft');
+  $newArticle.data('category', this.category);
+
+  /* TODO: Now use jQuery to fill in the rest of the current
+  template clone with properties from this particular Article instance.
+  We need to fill in:
+    1. author name,
+    2. author url,
+    3. article title,
+    4. article body, and
+    5. publication date. */
+    //these will be values
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('section').html(this.category);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('header a').attr('href', this.authorUrl);
+  $newArticle.find('section.article-body').html(this.body);
+  // console.log(this.authorUrl);
+  // console.log($newArticle);
+
+
+
+  // Display the date as a relative number of 'days ago'
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
+  $newArticle.append('<hr>');
+  return $newArticle;
+};
+
+rawData.sort(function (a, b) {
+  // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function (articleObject) {
+  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+  articles.push(new Article(articleObject));
+});
+
+articles.forEach(function (article) {
+  $('#articles').append(article.toHtml());
+});
---
+++ JS/blogarticles.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var rawData = [
+  {
+    title:       'About-Me',
+    category:    'basics',
+    author:      'Alex Cahn',
+    authorUrl:   'https://github.com/mypalal84?tab=repositories',
+    publishedOn: '2017-01-08',
+    body:        '<p>This was my very first project for my CodeFellows 201 class.  Back when we were learning the basics of html, css, and javascript. We have already come a long way since then.</p><p><img src="IMG/profile pic.jpg" class="pull-right" ></p>'
+  },
+  {
+    title:       'Take a Hike',
+    category:    'quiz',
+    author:      'Alex Cahn, Abigail White, Miguel Pena, Stephen Johnson',
+    authorUrl:   'https://github.com/abswhite1/take-a-hike/graphs/contributors',
+    publishedOn: '2017-01-27',
+    body:        '<p>This was our group project for the end of Code 201.  We made a website that gives the user a quiz complete with pictures to choose from.  At the end of the quiz, depending on your choices, a recommended hike is generated.</p><p> <img src="IMG/take-a-hike.png" href="https://abswhite1.github.io/take-a-hike/" class="pull-left" ></p>'
+  },
+];
