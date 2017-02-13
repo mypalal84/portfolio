82d1f68b95bad669dbfa6afdbbd0461cb52fce2c
Alex Cahn <alexcahn@gmail.com>
2017-02-12 22:17:14 -0800

    added handlebars

--- JS/app.js
+++ JS/app.js
@@ -13,31 +13,17 @@ function Article(opts) {
 }
 
 Article.prototype.toHtml = function () {
-  var $newArticle = $('article.template').clone().removeClass('template');
-
-  $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author);
-
-//   $newArticle.find('.byline a').text(this.author);
-//   $newArticle.find('.byline a').attr('href', this.authorUrl);
-//   $newArticle.find('h1:first').text(this.title);
-//   $newArticle.find('.article-body').html(this.body);
-//   $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
-//   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-//   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//   return $newArticle;
-// };
-  $newArticle.find('h1').html(this.title);
-  $newArticle.find('section').html(this.category);
-  $newArticle.find('a').html(this.author);
-  $newArticle.find('header a').attr('href', this.authorUrl);
-  $newArticle.find('section.article-body').html(this.body);
-
-  // Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
-  $newArticle.append('<hr>');
-  return $newArticle;
-  };
+  // Use handlebars to render your articles.
+  // Get your template from the DOM.
+  // "compile" your template with Handlebars.
+  var source = $('#articles-template').html();
+  var templateRender = Handlebars.compile(source);
+  console.log(templateRender);
+  return templateRender(this);
+  // Display how old the post was
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+};
 
 projects.sort(function (a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
